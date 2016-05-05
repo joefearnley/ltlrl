421bc0f5c8e60a865e77f0b7533eec53cf721213
Joe Fearnley <joe.fearnley@gmail.com>
2016-05-04 22:20:40 -0400

    Extracting out fetch method and adding delete method

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -10,52 +10,26 @@
                 </ul>
             </div>
             <div class="col-sm-9 col-md-10">
-                {{--<h1>Dashboard</h1>--}}
-                {{--<div class="row placeholders">--}}
-                    {{--<div class="col-xs-6 col-sm-3 placeholder">--}}
-                        {{--<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="--}}
-                             {{--width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">--}}
-                        {{--<h4>Label</h4>--}}
-                        {{--<span class="text-muted">Something else</span>--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-6 col-sm-3 placeholder">--}}
-                        {{--<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="--}}
-                             {{--width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">--}}
-                        {{--<h4>Label</h4>--}}
-                        {{--<span class="text-muted">Something else</span>--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-6 col-sm-3 placeholder">--}}
-                        {{--<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="--}}
-                             {{--width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">--}}
-                        {{--<h4>Label</h4>--}}
-                        {{--<span class="text-muted">Something else</span>--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-6 col-sm-3 placeholder">--}}
-                        {{--<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="--}}
-                             {{--width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">--}}
-                        {{--<h4>Label</h4>--}}
-                        {{--<span class="text-muted">Something else</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-
                 <urls list="{{ Auth::user()->urls }}"></urls>
 
                 <template id="urls-template">
-                    <h2 class="sub-header">Urls</h2>
-                    <ul class="list-group">
-                        <li class="list-group-item" v-for="url in list">
-                            <div class="row">
-                                <div class="col-sm-8">
-                                    <strong>Url:</strong> @{{ url.url }}<br>
-                                    <strong>Clicks:</strong> @{{ url.clicks }}
-                                </div>
-                                <div class="col-sm-4">
-                                    <button class="btn btn-primary"><i class="fa fa-pencil"></i> Edit</button>
-                                    <button class="btn btn-danger @click="delete(url)"><i class="fa fa-trash"></i> Delete</button>
+                    <div>
+                        <h2 class="sub-header">Urls</h2>
+                        <div class="list-group">
+                            <div class="list-group-item" v-for="url in list">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <strong>Url:</strong> @{{ url.url }}<br>
+                                        <strong>Clicks:</strong> @{{ url.clicks }} <br>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <button class="btn btn-primary"><i class="fa fa-pencil"></i> Edit</button>
+                                        <button class="btn btn-danger" @click="deleteUrl(url)"><i class="fa fa-trash"></i> Delete</button>
+                                    </div>
                                 </div>
                             </div>
-                        </li>
-                    </ul>
+                        </div>
+                    </div>
                 </template>
             </div>
         </div>
@@ -64,7 +38,7 @@
 
 
 @section('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.21/vue.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.21/vue.js"></script>
     <script>
         Vue.component('urls', {
             template: '#urls-template',
@@ -74,12 +48,15 @@
                 }
             },
             created: function() {
-                $.getJSON('/api/account/urls', function(urls) {
-                    this.list = urls;
-                }.bind(this));
+                this.fetchUrlList();
             },
             methods: {
-                delete: function(url) {
+                fetchUrlList: function() {
+                    $.getJSON('/api/account/urls', function(urls) {
+                        this.list = urls;
+                    }.bind(this));
+                },
+                deleteUrl: function(url) {
                     this.list.$remove(url);
                 }
             }
