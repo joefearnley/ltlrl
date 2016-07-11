7494ba30510ba02033b7eea39dc21ea4f8598f7a
Joe Fearnley <joe.fearnley@gmail.com>
2016-07-11 13:58:18 -0400

    Adding validation to main form

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,20 +3,8 @@
 @section('content')
     <div class="content">
         <div class="container content-page">
-            @if (count($errors) > 0)
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            @endif
-
+            <div id="error-message">
+            </div>
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 align-center">
                     <h1>Little URL</h1>
@@ -26,20 +14,22 @@
                 <div class="col-md-8 col-md-offset-2">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form id="url-form" action="url/create" method="post">
+                            <form id="url-form" action="url/create" method="post" class="form-horizontal">
                                 {!! csrf_field() !!}
-                                <div class="input-group input-group-lg">
-                                    <input type="text" name="url" id="url" class="form-control input-lg" placeholder="Enter Url and ...">
-                                    <span class="input-group-btn">
-                                        <button id="submit-form" class="btn btn-default ladda-button" data-style="expand-left"><span class="ladda-label">Make it Little</span></button>
-                                    </span>
+                                <div class="form-group">
+                                    <label id="url-error-message" class="control-label" for="url"></label>
+                                    <div class="input-group input-group-lg">
+                                        <input type="text" name="url" id="url" class="form-control input-lg" placeholder="Enter Url and ...">
+                                        <span class="input-group-btn">
+                                            <button id="submit-form" class="btn btn-default ladda-button" data-style="expand-left"><span class="ladda-label">Make it Little</span></button>
+                                        </span>
+                                    </div>
                                 </div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-
             <div id="response-message">
             </div>
         </div>
@@ -51,11 +41,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>@{{ error }}</li>
-                        @endforeach
-                    </ul>
+                    @{{ errorMessage }}
                 </div>
             </div>
         </div>
@@ -64,47 +50,71 @@
     <script id="response-message-template" type="x-tmpl-mustache">
         <div class="row result">
             <div class="col-md-8 col-md-offset-2">
-                <h4>URL has been made little! - <strong>@{{ link }}</strong></h4>
+                <h4>
+                    URL has been made little! - <strong>@{{ link }}</strong>
+                    <button id="copy-url" class="btn btn-success margin-left">
+                        <i class="fa fa-clipboard" aria-hidden="true"></i> Copy to Clipboard
+                    </button>
+                </h4>
             </div>
         </div>
     </script>
 
     <script>
-        var MainForm = {
+        var UrlForm = {
             init: function() {
                 this.bindEvents();
             },
             bindEvents: function() {
-                $('#submit-form').click(this.submitForm);
+                $(document).on('click', '#submit-form', this.submitForm);
+                $(document).on('click', '#copy-url', this.copyUrl);
             },
             submitForm: function(e) {
                 e.preventDefault();
+
+                var self = UrlForm;
+                self.hideError();
                 var l = Ladda.create(document.querySelector('#submit-form'));
 	 	        l.start();
 
                 var data = $('#url-form').serialize();
                 $.post('url/create', data)
                     .success(function(response) {
-                        if (response.success) {
-                            var template = $('#response-message-template').html();
-                            var html = Mustache.render(template, response.url);
-                            $('#response-message').html(html);
-                        } else {
-                            var template = $('#error-message-template').html();
-                            var html = Mustache.render(template, { 'error' : response.message});
-                            $('#response-message').html(html);
-                        }
+                        self.renderResponse('#response-message-template', '#response-message', response.url)
                     }, 'json')
+                    .error(function(response) {
+                        self.showError(response.responseJSON.url[0]);
+                    })
                     .always(function() {
                          l.stop();
                     });
 
                 return false;
+            },
+            renderResponse: function(templateSelector, messageSelector, data) {
+                var template = $(templateSelector).html();
+                var html = Mustache.render(template, data);
+                $(messageSelector).html(html);
+            },
+            showError: function(message) {
+                var $formButton = $('#submit-form');
+                $formButton.closest('.form-group').addClass('has-error');
+                $formButton.addClass('btn-danger');
+                $('#url-error-message').text(message);
+            },
+            hideError: function() {
+                var $formButton = $('#submit-form');
+                $formButton.closest('.form-group').removeClass('has-error');
+                $formButton.removeClass('btn-danger');
+                $('#url-error-message').text('');
+            },
+            copyUrl: function() {
+
             }
         };
 
         $(function() {
-            MainForm.init();
+            UrlForm.init();
         });
     </script>
 @endsection
