9f684e7699cdc24a3033d99686bde25deae444fc
Joe Fearnley <joe.fearnley@gmail.com>
2016-09-04 13:21:23 -0400

    turning debug bar off

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
