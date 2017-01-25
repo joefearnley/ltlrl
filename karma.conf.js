147d37899e8bd36667931a8b7fbbae8c82b766c1
Joe Fearnley <joe.fearnley@gmail.com>
2017-01-24 21:47:43 -0500

    Changing karma spec

--- karma.conf.js
+++ karma.conf.js
@@ -6,19 +6,19 @@ module.exports = function(config) {
             'public/js/lib.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'resources/assets/js/app/app.js',
-            'resources/assets/js/app/controllers/*.js',
+            'resources/assets/js/app/controllers/home.controller.js',
             //'resources/assets/js/app/services/*.js',
             'resources/assets/js/tests/*.js'
         ],
         exclude: [],
         preprocessors: {},
-        reporters: ['progress'],
+        reporters: ['spec'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['PhantomJS'],
-        singleRun: false,
+        singleRun: true,
         concurrency: Infinity
     });
 };
