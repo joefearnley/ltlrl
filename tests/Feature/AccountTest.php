5aabca2c35471b96ce27147ca52887b18d2d6e49
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-21 12:56:02 -0400

    Tests are green!

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -62,6 +62,7 @@ class AccountTest extends TestCase
 
         $this->actingAs($user)
             ->get('/account/settings')
+            ->assertStatus(200)
             ->assertSee('Account Settings')
             ->assertSee('Personal Information')
             ->assertSee('Update Password');
