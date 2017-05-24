635852c4115eedbcb9911a3ac33127f0070d05ca
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-23 22:26:11 -0400

    cleaning up javascript

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content" ng-app="home" ng-controller="HomeController">
+    <div class="content">
         <div class="container content-page home-page">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 align-center">
@@ -15,12 +15,15 @@
                             <form id="url-form" class="form-horizontal">
                                 {!! csrf_field() !!}
                                 <div class="form-group" ng-class="{ 'has-error': errorOccured }" >
-                                    <label class="control-label" for="url" ng-if="errorOccured">@{{ errorMessage }}</label>
+                                    <label class="control-label" for="url"></label>
                                     <div class="input-group input-group-lg">
-                                        <input type="text" name="url" id="url" ng-model="url" class="form-control input-lg" placeholder="Enter Url and ..." ng-keyup="removeErrorMessage()">
+                                        <input type="text" name="url" id="url" class="form-control input-lg" placeholder="Enter Url and ...">
                                         <span class="input-group-btn">
-                                            <button class="btn btn-default" ng-class="{ 'btn-danger': errorOccured }" ladda="loading" data-spinner-color="#FFFFFF" data-style="expand-left" ng-click="createUrl()">
+<!--                                             <button class="btn btn-default" ladda="loading" data-spinner-color="#FFFFFF" data-style="expand-left">
                                                 Make it Little
+                                            </button> -->
+                                            <button id="make-url-little" class="btn btn-default ladda-button" data-style="expand-left" data-spinner-color="#fff">
+                                                <span class="ladda-label">Make it Little</span>
                                             </button>
                                         </span>
                                     </div>
@@ -30,11 +33,11 @@
                     </div>
                 </div>
             </div>
-            <div class="row" ng-if="urlCreated">
+            <div class="row results">
                 <div class="col-md-8 col-md-offset-2">
                     <h4>
                         URL has been made little! - <strong>@{{ currentUrl }}</strong>
-                        <button id="copy-url" class="btn btn-success margin-left" ng-click="copyUrl()">
+                        <button id="copy-url" class="btn btn-success margin-left">
                             <i class="fa fa-clipboard" aria-hidden="true"></i> Copy to Clipboard
                         </button>
                     </h4>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,25 @@
 const { mix } = require('laravel-mix');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.scripts([
+    'resources/assets/js/lib/jquery.min.js',
+    'resources/assets/js/lib/bootstrap.min.js',
+    'resources/assets/js/lib/sweetalert2.min.js',
+    'resources/assets/js/lib/ladda.min.js',
+    'resources/assets/js/lib/spin.min.js'
+], 'public/js/lib.js');
+
+mix.scripts([
+    'resources/assets/js/app/home.js'
+], 'public/js/app.js');
+
+mix.styles([
+    'resources/assets/css/bootstrap.min.css',
+    'resources/assets/css/font-awesome.min.css',
+    'resources/assets/css/ladda-themeless.min.css',
+    'resources/assets/css/sweetalert2.min.css',
+    'resources/assets/css/main.css'
+], 'public/css/app.css');
+
+
+//   .sass('resources/assets/sass/app.scss', 'public/css');
