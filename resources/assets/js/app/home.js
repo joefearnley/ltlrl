43bf8f49e73fef709e7edcf7ad697da1190a9388
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-24 21:49:26 -0400

    I suppose we are going to go it alone

--- resources/assets/js/app/home.js
+++ resources/assets/js/app/home.js
@@ -1,4 +1,24 @@
 
+class HomeForm {
+    constructor() {
+        this.submitButton = document.querySelector('#make-url-little');
+        this.submitButton.addEventListener('click', this.createUrl);
+    }
+    createUrl () {}
+    handleError() {}
+    showResult () {}
+}
+
+var createUrl = () => {
+    featch('/url/create')
+        .then(response => {
+            return response.json();
+        }).then(response => {
+        }).catch(error => {
+            console.log(error);
+        });
+};
+
 (function() {
     const submitButton = document.querySelector('#make-url-little');
     const l = Ladda.create(submitButton);
