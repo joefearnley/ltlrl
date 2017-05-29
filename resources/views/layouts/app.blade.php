13b0b9c74a29b51cd64c6eb9599cd3683e6d018b
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-29 08:35:57 -0400

    Migrating to bulma

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,43 +10,34 @@
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="nav">
-        <div class="nav-left">
-            <a class="nav-item" href="{{ url('/') }}">Little Url</a>
-        </div>
-        <span class="nav-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </span>
-        <div class="nav-right nav-menu">
-        <a class="nav-item">
-            Home
-        </a>
-        @if (Auth::guest())
-            <a href="{{ url('/login') }}" class="nav-item">Login</a>
-            <a href="{{ url('/register') }}" class="nav-item">Register</a>
-        @else
-            @if (Auth::check() && Route::getCurrentRoute()->uri() != '/')
-                <p class="control">
-                    <a  id="show-add-url-form"class="button is-primary">
-                        <span class="icon"><i class="fa fa-plus"></i></span>
-                        <span>Make Little Url</span>
-                    </a>
-                </p>
+    <div class="container">
+        <nav class="nav">
+            <div class="nav-left">
+                <a class="nav-item" href="{{ url('/') }}">Little Url</a>
+            </div>
+            <span class="nav-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+            <div class="nav-right nav-menu">
+            @if (Auth::guest())
+                <a href="{{ url('/login') }}" class="nav-item">Login</a>
+                <a href="{{ url('/register') }}" class="nav-item">Register</a>
+            @else
+                @if (Auth::check() && Route::getCurrentRoute()->uri() != '/')
+                    <p class="control">
+                        <a  id="show-add-url-form" class="button is-primary">
+                            <span class="icon"><i class="fa fa-plus"></i></span>
+                            <span>Make Little Url</span>
+                        </a>
+                    </p>
+                @endif
+                <a href="{{ url('/account') }}" class="nav-item">Account</a>
+                <a href="{{ url('/logout') }}" class="nav-item">Logout</a>
             @endif
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="{{ url('/account') }}"><i class="fa fa-btn fa-user"></i> Account</a></li>
-                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
-                </ul>
-            </li>
-        @endif
-    </nav>
-
+        </nav>
+    </div>
     @yield('content')
     <script src="/js/lib.js"></script>
     <script src="/js/app.js"></script>
