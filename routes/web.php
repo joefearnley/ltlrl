a63e48822f78e61101dcdb78c063f4f7d217f42d
Joe Fearnley <joe.fearnley@gmail.com>
2018-02-14 21:38:37 -0500

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('account', 'AccountController@index');
+Route::get('account/urls', 'AccountController@urlList');
+Route::get('account/settings', 'AccountController@settings');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/{key}', 'HomeController@redirect');
+
+Route::get('url/{id}', 'UrlController@show');
+Route::post('url/create', 'UrlController@create');
+Route::post('url/update', 'UrlController@update');
+Route::post('url/delete/{id}', 'UrlController@delete');
+Route::get('url/stats/{id}', 'UrlController@clickStats');
+
+Route::get('/api/account/urls', 'AccountController@urls');
+Route::get('/api/account/info', 'AccountController@info');
+Route::post('/api/account/update-personal-info', 'AccountController@updatePersonalInfo');
+Route::post('/api/account/update-password', 'AccountController@updatePassword');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
