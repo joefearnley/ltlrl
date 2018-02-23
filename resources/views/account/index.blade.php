bdb049680643bd498c618931070479b7649b6439
Joe Fearnley <joe.fearnley@gmail.com>
2018-02-23 13:35:14 -0500

    Updating main account page

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,49 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="app" class="container-fluid">
-        <div class="row">
-            @include('account.nav')
-            <div class="col-sm-9 col-md-10 main">
-                <h2>Account Overview</h2>
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="tile tile-dark-blue">
-                            <div class="img">
-                                <i class="fa fa-calendar"></i>
-                            </div>
-                            <div class="content">
-                                <p class="big counter">{{ $user->daysMakingUrlsLittle() }}</p>
-                                <p class="title">Days Making Urls Little</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="tile tile-dark-blue">
-                            <div class="img">
-                                <i class="fa fa-link"></i>
-                            </div>
-                            <div class="content">
-                                <p class="big counter">{{ $user->urlsMade() }}</p>
-                                <p class="title">Urls Made Little</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="tile tile-dark-blue">
-                            <div class="img">
-                                <i class="fa fa-mouse-pointer"></i>
-                            </div>
-                            <div class="content">
-                                <p class="big counter">{{ $user->urlsClickedOn() }}</p>
-                                <p class="title">Urls Clicked On</p>
-                            </div>
-                        </div>
-                    </div>
+<div class="container">
+    <div class="columns">
+    @include('account.nav')
+    <div class="column is-9">
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+            <h1>Account Overview</h1>
+        </nav>
+        <section class="hero is-primary welcome is-small">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">Hello, {{ $user->name }}.</h1>
+                    <h2 class="subtitle">I hope you are having a great day!</h2>
                 </div>
             </div>
-        </div>
+        </section>
+        <section class="info-tiles stats">
+            <div class="tile is-ancestor has-text-centered">
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title">{{ $user->daysMakingUrlsLittle() }}</p>
+                        <p class="subtitle">Days Making Urls Little</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <p class="title">{{ $user->urlsMade() }}</p>
+                            <p class="subtitle">Urls Made Little</p>
+                        </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title">{{ $user->urlsClickedOn() }}</p>
+                        <p class="subtitle">Urls Clicked on</p>
+                    </article>
+                </div>
+            </div>
+        </section>
     </div>
+</div>
 @endsection
 
 @section('scripts')
