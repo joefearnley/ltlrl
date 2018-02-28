8a4c85c74cc5c1aaf30a6ef556a8481f8bccab65
Joe Fearnley <joe.fearnley@gmail.com>
2018-02-28 15:00:23 -0500

    Updating admin nav menu

---
+++ resources/views/account/nav.blade.php
@@ -0,0 +1,24 @@
+<div class="column is-one-fifth sidebar">
+    <aside class="menu">
+        <p class="menu-label">
+            {{request()->path()}}
+        </p>
+        <ul class="menu-list">
+            <li>
+                <a href="/account" class="{{ request()->path() == 'account' ? 'is-active' : '' }}">
+                    <i class="fas fa-tachometer-alt"></i> Account Overview <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li>
+                <a href="/account/urls" class="{{ request()->path() == 'account/urls' ? 'is-active' : '' }}">
+                    <i class="fas fa-list"></i> Urls
+                </a>
+            </li>
+            <li>
+                <a href="/account/settings" class="i{{ request()->path() == 'account/settings' ? 'is-active' : '' }}">
+                    <i class="fas fa-cog"></i> Settings
+                </a>
+            </li>
+        </ul>
+    </aside>
+</div>
