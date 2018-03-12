eb14cc921e7ae1c3e44acc14cb170796ce0acce1
Joe Fearnley <joe.fearnley@gmail.com>
2018-03-12 19:23:43 -0400

    Adding personal information form

---
+++ resources/assets/js/components/PersonalInformationForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="columns m-t-lg">
+        <div class="column is-6">
+            <div class="panel">
+                <p class="panel-heading">
+                    Update Personal Information
+                </p>
+                <div class="panel-block">
+                    <form>
+                        <input type="hidden" name="id" value="{{ user.id }}">
+                        <div class="field">
+                            <label class="label">Name</label>
+                            <div class="control">
+                                <input type="text" id="name" class="input" name="name" placeholder="Name" v-model="name" required>
+                                <div class="help-block with-errors" v-show="nameError" v-model="nameErrorMessage"></div>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">E-Mail Address</label>
+                            <div class="control">
+                                <input type="email" id="email" class="input" name="email"  placeholder="E-Mail Address" v-model="email" required>
+                                <div class="help-block with-errors" v-show="emailError" v-model="emailErrorMessage"></div>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-primary" @click="updateUserInformation()">
+                                    <i class="fa fa-btn fa-save"></i> Save
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: {
+            name: '',
+            email: ''
+            nameError: false,
+            nameErrorMessage: '',
+            emailError: false,
+            emailErrorMessage: ''
+        }
+        mounted() {
+            this.getUser();
+        },
+        methods: {
+            getUserInformation () {
+                axios.get('/user')
+                    .then(response => {
+                        console.log(response);
+                    })
+                    .catch(error => showErrorMessage(error.response.data.message));
+            }
+            updateUserInformation () {
+            },
+            showErrorMessage () {
+
+            }
+        }
+    }
+</script>
--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -6,42 +6,7 @@
             @include('account.nav')
             <div class="column m-t-md">
                 <h1 class="title">Account Settings</h1>
-                <div class="columns m-t-lg">
-                    <div class="column is-6">
-                        <div class="panel">
-                            <p class="panel-heading">
-                                Personal Information
-                            </p>
-                            <div class="panel-block">
-                                <form>
-                                    {!! csrf_field() !!}
-                                    <input type="hidden" name="id" value="{{ $user->id }}">
-                                    <div class="field">
-                                        <label class="label">Name</label>
-                                        <div class="control">
-                                            <input type="text" id="name" class="input" name="name" placeholder="Name" ng-model="name" required>
-                                            <div class="help-block with-errors"></div>
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <label class="label">E-Mail Address</label>
-                                        <div class="control">
-                                            <input type="email" id="email" class="input" name="email"  placeholder="E-Mail Address" data-error="Email address is invalid" required>
-                                            <div class="help-block with-errors"></div>
-                                        </div>
-                                    </div>
-                                    <div class="field">
-                                        <div class="control">
-                                            <button class="button is-primary">
-                                                <i class="fa fa-btn fa-save"></i> Save
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <personal-information-form></personal-information-form>
                 <div class="columns m-t-lg">
                     <div class="column is-6">
                         <div class="panel">
