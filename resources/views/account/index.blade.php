dc1fd85aeaed7c053b81e54dbb35cdcb1bc32adf
Joe Fearnley <joe.fearnley@gmail.com>
2018-03-19 17:00:35 -0400

    Added missing div tag that was causing Vue to throw an error

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -3,38 +3,39 @@
 @section('content')
 <div class="container">
     <div class="columns">
-    @include('account.nav')
-    <div class="column is-9 m-t-md">
-        <section class="hero is-primary welcome is-small">
-            <div class="hero-body">
-                <div class="container">
-                    <h1 class="title">Hello, {{ $user->name }}.</h1>
-                    <h2 class="subtitle">I hope you are having a great day!</h2>
+        @include('account.nav')
+        <div class="column is-9 m-t-md">
+            <section class="hero is-primary welcome is-small">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="title">Hello, {{ $user->name }}.</h1>
+                        <h2 class="subtitle">I hope you are having a great day!</h2>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section class="info-tiles stats">
-            <div class="tile is-ancestor has-text-centered">
-                <div class="tile is-parent">
-                    <article class="tile is-child box">
-                        <p class="title">{{ $user->daysMakingUrlsLittle() }}</p>
-                        <p class="subtitle">Days Making Urls Little</p>
-                    </article>
+            </section>
+            <section class="info-tiles stats">
+                <div class="tile is-ancestor has-text-centered">
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <p class="title">{{ $user->daysMakingUrlsLittle() }}</p>
+                            <p class="subtitle">Days Making Urls Little</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <p class="title">{{ $user->urlsMade() }}</p>
+                            <p class="subtitle">Urls Made Little</p>
+                        </article>
+                    </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                            <p class="title">{{ $user->urlsClickedOn() }}</p>
+                            <p class="subtitle">Urls Clicked On</p>
+                        </article>
+                    </div>
                 </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box">
-                        <p class="title">{{ $user->urlsMade() }}</p>
-                        <p class="subtitle">Urls Made Little</p>
-                    </article>
-                </div>
-                <div class="tile is-parent">
-                    <article class="tile is-child box">
-                        <p class="title">{{ $user->urlsClickedOn() }}</p>
-                        <p class="subtitle">Urls Clicked On</p>
-                    </article>
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 </div>
 @endsection
