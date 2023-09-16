c7af681604c51b8573fb6f0b36d00b8820c72ba6
Joe Fearnley <joe.fearnley@gmail.com>
2023-09-16 17:49:01 -0400

    Setting up Url model and rest stuff

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \Lomkit\Rest\Facades\Rest;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Rest::resource('users', \App\Http\Controllers\UsersController::class);
+Rest::resource('urls', \App\Rest\Resources\UrlController::class);
