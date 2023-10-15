d8b9a74a265fb60a92455d8350a9e42d665c9ed9
Joe Fearnley <joe.fearnley@gmail.com>
2023-10-14 21:32:33 -0400

    add user api resource

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,13 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UrlController;
 
+// need for the next api auth check
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
 Route::apiResources([
-    // 'users' => UserController::class,
+    'users' => UserController::class,
     'urls' => UrlController::class,
 ]);
 
