a26cfb530f0802fd171a4ac47e2026a0d57de4e6
Joe Fearnley <joe.fearnley@gmail.com>
2023-10-20 18:41:16 -0400

    Adding copied text to copy button

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -106,9 +106,6 @@ export default function Home() {
                     }
                 </div>
             </div>
-            <div id="toast-bottom-left" className="fixed flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow bottom-5 left-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
-                <div className="text-sm font-normal">Bottom left positioning.</div>
-            </div>
         </>
     )
 }
