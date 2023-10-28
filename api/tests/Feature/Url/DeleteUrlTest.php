48d829b6570037f9f9f94d0e771122f234ca91ee
Joe Fearnley <joe.fearnley@gmail.com>
2023-10-28 11:11:08 -0400

    Adding Delette button and tests

---
+++ api/tests/Feature/Url/DeleteUrlTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Url;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Url;
+
+class DeleteUrlTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_cannot_delete_url_when_unauthenticated(): void
+    {
+        $url = Url::factory()->create();
+
+        $this->deleteJson(route('urls.destroy', ['url' => $url->id]))
+            ->assertStatus(401);
+    }
+
+    public function test_can_delete_url(): void
+    {
+        $user = User::factory()->create();
+        $url = Url::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->actingAs($user)
+            ->deleteJson(route('urls.destroy', ['url' => $url->id]))
+            ->assertStatus(200);
+
+        $this->assertDatabaseMissing('urls', [
+            'id' => $url->id,
+            'title' => $url->title,
+            'url' => $url->url,
+            'user_id' => $user->id,
+        ]);
+    }
+}
--- client/src/components/Url/DeleteUrlButton.js
+++ client/src/components/Url/DeleteUrlButton.js
@@ -6,24 +6,21 @@ import CopyToClipboard from '@/components/CopytoClipboard'
 import axios from '@/lib/axios'
 import { useState } from 'react'
 
-const DeleteUrlButton = ({ className }) => {
-    const [title, setTitle] = useState('')
-    const [url, setUrl] = useState('')
+const DeleteUrlButton = ({ urlId }) => {
     const [showModal, setShowModal] = useState(false)
     const [errors, setErrors] = useState([])
-    const [littleUrl, setLittleUrl] = useState('')
+
     const csrf = () => axios.get('/sanctum/csrf-cookie')
 
-    const openCreateUrlModal = () => {
+    const openDeleteUrlModal = () => {
         setShowModal(true)
     }
 
-    const hideCreateUrlModal = () => {
+    const hideDeleteUrlModal = () => {
         setShowModal(false)
-        window.location.reload()
     }
 
-    const submitForm = async event => {
+    const deleteUrl = async event => {
         event.preventDefault()
 
         await csrf()
@@ -31,14 +28,11 @@ const DeleteUrlButton = ({ className }) => {
         setErrors([])
 
         axios
-            .post('/api/urls', {
-                title,
-                url
+            .delete('/api/urls', {
+                urlId
             })
             .then(response => {
-                if (response.data) {
-                    setLittleUrl(response.data.data.little_url)
-                }
+                window.location.reload()
             })
             .catch(error => {
                 console.log(error)
@@ -50,89 +44,44 @@ const DeleteUrlButton = ({ className }) => {
     return (
         <>
             <Button
-                className={`${className} m-auto`}
-                onClick={openCreateUrlModal}
+                onClick={openDeleteUrlModal}
+                className="ml-3 w-25 justify-center bg-persian_red hover:bg-persian_red-400 active:bg-persian_red focus:border-persian_red"
             >
-                Create Url
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
+                Delete
             </Button>
             <div className={`relative z-10 ${showModal ? 'block' : 'hidden'}`} aria-labelledby="modal-title" role="dialog" aria-modal="true">
                 <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
                 <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                     <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                         <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                        {littleUrl ? (
-                            <>
-                                <div className="px-4 py-3 text-center sm:mt-0 sm:text-left">
-                                    <h3 className="text-base font-semibold leading-6 text-gray-900">Url Sucessfully Created</h3>
-                                </div>
-                                <div className="flex items-center justify-between w-full p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow bottom-5 left-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800">
-                                    <span>{littleUrl}</span>
-                                    <CopyToClipboard copyText={littleUrl} />
-                                </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <Button className="ml-3 w-25 justify-center bg-dark_slate_gray hover:bg-dark_slate_gray-400 active:bg-dark_slate_gray focus:border-dark_slate_gray" onClick={hideCreateUrlModal}>Close</Button>
-                                </div>
-                            </>
-                        ) : (
-                            <form onSubmit={submitForm}>
-                                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                    <div className="sm:flex sm:items-start">
-                                        <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                                            <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Create Little URL</h3>
-                                            <div className="mt-2">
-                                                <p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
-                                            </div>
-                                            {/* URL Title */}
-                                            <div className="mt-4">
-                                                <Label htmlFor="title">Title</Label>
-
-                                                <Input
-                                                    id="title"
-                                                    type="text"
-                                                    value={title}
-                                                    className="block mt-1 w-full"
-                                                    onChange={event => setTitle(event.target.value)}
-                                                    required
-                                                    autoFocus
-                                                />
-
-                                                <InputError messages={errors.email} className="mt-2" />
-                                            </div>
-
-                                            {/* URL */}
-                                            <div className="mt-4 mb-4">
-                                                <Label htmlFor="password">URL</Label>
-
-                                                <Input
-                                                    id="url"
-                                                    type="url"
-                                                    value={url}
-                                                    className="block mt-1 w-full"
-                                                    onChange={event => setUrl(event.target.value)}
-                                                    required
-                                                    autoComplete="current-url"
-                                                />
-
-                                                <InputError messages={errors.email} className="mt-2" />
-                                            </div>
+                            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div className="sm:flex sm:items-start">
+                                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                        </svg>
+                                    </div>
+                                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                        <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Delete URL</h3>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">Are you sure you want to delete this URL?</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                    <Button className="ml-3 w-25 justify-center">Save</Button>
-                                    <Button className="ml-3 w-25 justify-center bg-dark_slate_gray hover:bg-dark_slate_gray-400 active:bg-dark_slate_gray focus:border-dark_slate_gray" onClick={hideCreateUrlModal}>Cancel</Button>
-                                </div>
-                            </form>
-                        )}
+                            </div>
+                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" onClick={deleteUrl}>Delete</button>
+                                <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" onClick={hideDeleteUrlModal}>Cancel</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </>
     )
-
+}
 
 export default DeleteUrlButton
