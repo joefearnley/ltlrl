d71ea4ebad68f4e46f62fc4a7686b41c364bb04a
Joe Fearnley <joe.fearnley@gmail.com>
2023-11-15 21:20:28 -0500

    Adding Url service

--- app/app/Http/Controllers/WelcomeController.php
+++ app/app/Http/Controllers/WelcomeController.php
@@ -15,4 +15,24 @@ class WelcomeController extends Controller
     {
         return view('welcome');
     }
+
+
+    /**
+     * Create a new Url in storage.
+     */
+    public function creteUrl(StoreUrlRequest $request)
+    {
+        $hashids = new Hashids('', 6);
+
+        $url = Url::create([
+            'title' => $request->title,
+            'url' => $request->url,
+            'user_id' => !is_null($request->user()) ? $request->user()->id : null,
+        ]);
+
+        $url->key = $hashids->encode($url->id);
+        $url->save();
+
+        return redirect('home')->with('url', $url);
+    }
 }
