b76da4fda9d47d6287c8ff9fe57701c17b049eb2
Joe Fearnley <joe.fearnley@gmail.com>
2023-11-21 21:20:04 -0500

    Roughing copy to clipboard functionality

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+const copyToClipboarButton = document.querySelector('.copy-to-clipboard-button');
+const newUrlInput = document.querySelector('#new_url');
+
+copyToClipboarButton.addEventListener('click', event => {
+    navigator.clipboard.writeText(newUrlInput.value);
+
+    // let user know it has been copied
+});
