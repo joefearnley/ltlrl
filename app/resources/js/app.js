0967cfad95c27510a44ee57f758be771263ad51b
Joe Fearnley <joe.fearnley@gmail.com>
2023-11-23 09:18:06 -0500

    Updating README and moving test DB to memory

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -8,11 +8,9 @@ Alpine.start();
 
 
 async function copyTextToClipboard(text) {
-    console.log("copying to clipboard")
     if ('clipboard' in navigator) {
         return await navigator.clipboard.writeText(text)
     } else {
-        console.log('no clipboard in navigator');
         return document.execCommand('copy', true, text)
     }
 }
@@ -22,8 +20,9 @@ const newUrlLink = document.querySelector('#new-url-link');
 
 copyToClipboardButton.addEventListener('click', event => {
     copyTextToClipboard(newUrlLink.innerHTML.trim())
-        .then(() => {
-            console.log('soifjowiejfwoefij');
+        .then(result => {
+            console.log(result);
+
 
             const currentCopyButtonHTML = copyToClipboardButton.innerHTML;
             copyToClipboardButton.innerHTML = 'Copied!';
