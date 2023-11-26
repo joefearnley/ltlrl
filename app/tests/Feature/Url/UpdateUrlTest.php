9d9e3b6575511a1c08a2e4a0368b1d4b7105535a
Joe Fearnley <joe.fearnley@gmail.com>
2023-11-26 11:42:04 -0500

    Adding update url tests

--- app/tests/Feature/Url/UpdateUrlTest.php
+++ app/tests/Feature/Url/UpdateUrlTest.php
@@ -5,12 +5,15 @@ namespace Tests\Feature\Url;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Url;
 
 class UpdateUrlTest extends TestCase
 {
     use RefreshDatabase;
 
     protected $user;
+
+    protected $url;
 
     protected function setUp(): void
     {
@@ -21,80 +24,101 @@ class UpdateUrlTest extends TestCase
 
     public function test_cannot_update_url_when_not_authenticated(): void
     {
-        $url = 'https://www.google.com';
+        $url = Url::factory()->create();
 
         $formData = [
-            'url' => $url,
+            'title' => $url->title,
+            'url' => $url->url,
         ];
 
-        $this->post(route('urls.store'), $formData)
+        $this->post(route('urls.store', $url), $formData)
             ->assertStatus(302)
             ->assertRedirect(route('welcome'));
     }
 
-    public function test_cannot_update_url_with_no_form_data()
+    public function test_cannot_update_url_with_no_form_data(): void
     {
+        $url = Url::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
         $formData = [];
 
         $this->actingAs($this->user)
-            ->post(route('urls.store'), $formData)
+            ->from(route('urls.store'))
+            ->post(route('urls.store', $url), $formData)
             ->assertStatus(302)
-            ->assertSessionHasErrors(['url' => 'The url field is required.']);
+            ->assertSessionHasErrors(['url' => 'The url field is required.'])
+            ->assertRedirect(route('urls.store'));
     }
 
-    public function test_cannot_update_url_with_no_url()
+    public function test_cannot_update_url_with_no_url(): void
     {
+        $url = Url::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
         $formData = [
             'url' => ''
         ];
 
         $this->actingAs($this->user)
-            ->post(route('urls.store'), $formData)
+            ->from(route('urls.store'))
+            ->post(route('urls.store', $url), $formData)
             ->assertStatus(302)
-            ->assertSessionHasErrors(['url' => 'The url field is required.']);
+            ->assertSessionHasErrors(['url' => 'The url field is required.'])
+            ->assertRedirect(route('urls.store'));
     }
 
-    public function test_can_create_url(): void
+    public function test_can_update_url(): void
     {
-        $url = 'https://www.google.com';
-
-        $formData = [
-            'url' => $url,
-        ];
-
-        $this->actingAs($this->user)
-            ->post(route('urls.store'), $formData)
-            ->assertStatus(302)
-            ->assertSessionHas('message')
-            ->assertSessionHas('littleUrl')
-            ->assertRedirect();
-
-        $this->assertDatabaseHas('urls', [
-            'url' => $url,
+        $url = Url::factory()->create([
             'user_id' => $this->user->id,
         ]);
-    }
 
-    public function test_can_create_title_and_url(): void
-    {
-        $title = 'Google';
-        $url = 'https://www.google.com';
+        $newUrl = 'https://yahoo.com';
 
         $formData = [
-            'title' => $title,
-            'url' => $url,
+            'url' => $newUrl,
         ];
 
         $this->actingAs($this->user)
-            ->post(route('urls.store'), $formData)
+            ->post(route('urls.store', $url), $formData)
             ->assertStatus(302)
             ->assertSessionHas('message')
             ->assertSessionHas('littleUrl')
             ->assertRedirect(route('urls.index'));
 
         $this->assertDatabaseHas('urls', [
-            'url' => $url,
-            'title' => $title,
+            'url' => $newUrl,
+            'user_id' => $this->user->id,
+        ]);
+    }
+
+    public function test_can_update_title_and_url(): void
+    {
+        $url = Url::factory()->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        $newTitle = 'Google';
+        $newUrl = 'https://www.google.com';
+
+        $formData = [
+            'title' => $newTitle,
+            'url' => $newUrl,
+        ];
+
+        $this->actingAs($this->user)
+            ->post(route('urls.store', $url), $formData)
+            ->assertStatus(302)
+            ->assertSessionHas('message')
+            ->assertSessionHas('littleUrl')
+            ->assertRedirect(route('urls.index'));
+
+        $this->assertDatabaseHas('urls', [
+            'title' => $newTitle,
+            'url' => $newUrl,
             'user_id' => $this->user->id,
         ]);
     }
