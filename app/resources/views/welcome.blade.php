968c2c0ad07fa3c03baca046b9a852338fabb9cc
Joe Fearnley <joe.fearnley@gmail.com>
2023-12-02 17:30:32 -0500

    Removing red outline from welcome page links

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 
     <div class="relative sm:flex sm:justify-center sm:items-center bg-center selection:text-white">
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 hover:underline focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 hover:underline focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 hover:underline focus:outline focus:outline-2 focus:rounded-sm">Log in</a>
+            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 hover:underline focus:outline focus:outline-2 focus:rounded-sm">Register</a>
         </div>
     </div>
 
