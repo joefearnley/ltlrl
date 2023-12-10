2c7a60de60b85a557b2e79ffffb67f3165a5d6c2
Joe Fearnley <joe.fearnley@gmail.com>
2023-12-10 15:23:12 -0500

    Small UI tweaks

--- app/resources/views/components/danger-button.blade.php
+++ app/resources/views/components/danger-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-sm text-white hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-persian_red-600 hover:bg-persian_red-700 inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-sm text-white focus:outline-none focus:ring-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
--- app/resources/views/urls/url-list.blade.php
+++ app/resources/views/urls/url-list.blade.php
@@ -30,10 +30,15 @@
                             <h3 class="text-2xl font-extrabold pb-6">
                                 {{ $url->title }}
                             </h3>
-                            <p class="py-1">
+                            <p class="py-1 flex gap-2">
                                 <a class="text-md text-gray-900" href="{{ $url->little_url }}">
                                     {{ $url->little_url }}
                                 </a>
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cursor-pointer">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                                    </svg>
+                                </button>
                             </p>
                             <p class="py-1">
                                 <a class="text-md text-gray-500" href="{{ $url->url }}">
@@ -41,40 +46,51 @@
                                 </a>
                             </p>
                         </div>
-                        <div>
-                            <a href="{{ route('urls.edit', $url) }}" class="block mt-4 mb-4 text-white bg-dark_slate_gray-700 hover:bg-dark_slate_gray-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
-                                {{ __('Edit') }}
-                            </a>
-                            <x-danger-button
-                                class="bg-persian_red-700 hover:bg-persian_red-800"
-                                x-data=""
-                                x-on:click.prevent="$dispatch('open-modal', 'confirm={{ $url->id }}-url-deletion')">
-                                {{ __('Delete') }}
-                        </x-danger-button>
-                            <x-modal name="confirm={{ $url->id }}-url-deletion" :show="false" focusable>
-                                <form method="post" action="{{ route('urls.destroy', $url) }}" class="p-6">
-                                    @csrf
-                                    @method('delete')
+                        <div class="flex gap-8 align-middle justify-center items-center">
+                            <div>
+                                {!! QrCode::size(80)->generate($url->little_url); !!}
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <a href="{{ route('urls.edit', $url) }}" class=" text-white bg-dark_slate_gray-700 hover:bg-dark_slate_gray-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
+                                    {{ __('Edit') }}
+                                </a>
+                                <x-danger-button
+                                    x-data=""
+                                    x-on:click.prevent="$dispatch('open-modal', 'confirm={{ $url->id }}-url-deletion')">
+                                    {{ __('Delete') }}
+                                </x-danger-button>
+                                <x-modal name="confirm={{ $url->id }}-url-deletion" :show="false" focusable>
+                                    <form method="post" action="{{ route('urls.destroy', $url) }}" class="p-6">
+                                        @csrf
+                                        @method('delete')
 
-                                    <h2 class="text-lg font-medium text-gray-900">
-                                        {{ __('Are you sure you want to delete this Url?') }}
-                                    </h2>
+                                        <h2 class="text-lg font-medium text-gray-900">
+                                            {{ __('Are you sure you want to delete this Url?') }}
+                                        </h2>
 
-                                    <div class="mt-6 flex justify-end">
-                                        <x-secondary-button x-on:click="$dispatch('close')">
-                                            {{ __('Cancel') }}
-                                        </x-secondary-button>
+                                        <div class="mt-6 flex justify-end">
+                                            <x-secondary-button x-on:click="$dispatch('close')">
+                                                {{ __('Cancel') }}
+                                            </x-secondary-button>
 
-                                        <x-danger-button class="ml-3">
-                                            {{ __('Delete Url') }}
-                                        </x-danger-button>
-                                    </div>
-                                </form>
-                            </x-modal>
+                                            <x-danger-button class="ml-3">
+                                                {{ __('Delete Url') }}
+                                            </x-danger-button>
+                                        </div>
+                                    </form>
+                                </x-modal>
+                            </div>
                         </div>
                     </div>
                     <hr class="block mt-4 mb-4">
-                    <p>{{ $url->created_at->format('M j, Y') }}</p>
+                    <div class="flex gap-2">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+                        </div>
+                        <div>{{ $url->created_at->format('M j, Y g:i:s a') }}</div>
+                    </p>
                 </div>
             </div>
         </div>
