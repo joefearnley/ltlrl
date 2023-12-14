98bbf777353a78e58212c3e9547521476bb58967
Joe Fearnley <joe.fearnley@gmail.com>
2023-12-13 21:36:38 -0500

    adding copy to clipboard

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -7,37 +7,40 @@ window.Alpine = Alpine;
 Alpine.start();
 
 
-async function copyTextToClipboard(text) {
-    if ('clipboard' in navigator) {
-        return await navigator.clipboard.writeText(text)
-    } else {
-        return document.execCommand('copy', true, text)
-    }
-}
-
 const copyToClipboardButtons = document.querySelectorAll('button.copy-to-clipboard');
-// const newUrlLink = document.querySelector('#new-url-link');
-
 
 copyToClipboardButtons.forEach(button => {
     button.addEventListener('click', event => {
-        console.log('clicked...');
-        console.log(button.previousElementSibling.innerHTML.trim());
+        const copyText = button.previousElementSibling.innerHTML.trim();
 
-        copyTextToClipboard(button.previousElementSibling.innerHTML.trim())
-            .then(result => {
-                console.log(result);
+        if ('clipboard' in navigator) {
+            navigator.clipboard.writeText(copyText)
+                .then(() => {
+                    console.log('Text copied');
+                })
+                .catch((err) => console.error(err.name, err.message));
 
-                const currentCopyButtonHTML = button.innerHTML;
-                button.innerHTML = 'Copied!';
+        } else {
+            const textArea = document.createElement('textarea');
+            textArea.value = copyText;
+            textArea.style.opacity = 0;
+            document.body.appendChild(textArea);
+            textArea.select();
 
-                setTimeout(() => {
-                    button.innerHTML = currentCopyButtonHTML;
-                }, '3000');
-            })
-            .catch((err) => {
-                console.log('error copying to clipboard:');
-                console.log(err);
-            });
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                console.error(err.name, err.message);
+            }
+
+            document.body.removeChild(textArea);
+        }
+
+        const currentCopyButtonHTML = button.innerHTML;
+        button.innerHTML = 'Copied!';
+
+        setTimeout(() => {
+            button.innerHTML = currentCopyButtonHTML;
+        }, '3000');
     });
 });
