8e18a6879a70bd0fa8563007c6bd2867203a881b
Joe Fearnley <joe.fearnley@gmail.com>
2023-12-17 21:48:46 -0500

    Adding more dashboarc tests

--- app/tests/Feature/DashboardTest.php
+++ app/tests/Feature/DashboardTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Models\Url;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Url;
+use App\Models\Click;
 
 class DashboardTest extends TestCase
 {
@@ -57,7 +58,6 @@ class DashboardTest extends TestCase
             ->assertStatus(200)
             ->assertSee('Latest Urls');
 
-
         $data = $response->getOriginalContent()->getData();
 
         $this->assertGreaterThan(0, $data['recentUrls']->count());
@@ -73,6 +73,36 @@ class DashboardTest extends TestCase
             ->assertSee('Click on the Create Url link to get started.');
     }
 
+    public function test_dashboard_contains_latest_activity()
+    {
+        $urls = Url::factory()->count(5)->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        $urlsClicked = $urls->take(3);
+
+        foreach($urlsClicked as $url) {
+            Click::factory()->create([
+                'url_id' => $url->id,
+            ]);
+        }
+
+        $response = $this->actingAs($this->user)
+            ->get(route('dashboard'))
+            ->assertStatus(200)
+            ->assertSee('Latest Activity');
+
+
+        foreach($urlsClicked as $url) {
+            $response->assertSee($url->title)
+                ->assertSee($url->url)
+                ->assertSee($url->little_url);
+        }
+
+        $data = $response->getOriginalContent()->getData();
+
+        $this->assertEquals(3, $data['latestClicks']->count());
+    }
 
     public function test_dashboard_contains_no_most_active_urls()
     {
@@ -82,5 +112,40 @@ class DashboardTest extends TestCase
             ->assertSee('Latest Activity')
             ->assertSee('No Active Urls Yet!')
             ->assertSee('Click on the Create Url link to get started.');
+    }
+
+    public function test_dashboard_contains_most_active_urls()
+    {
+        $urls = Url::factory()->count(15)->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        $urlsClicked = $urls->take(5);
+
+        foreach($urlsClicked as $url) {
+            Click::factory()->count(2)->create([
+                'url_id' => $url->id,
+            ]);
+        }
+
+        // create clicks on 10 of these urls 5 on one
+        // assert the count is 10
+        // assert these urls' data and click count shows on on page
+
+        $response = $this->actingAs($this->user)
+            ->get(route('dashboard'))
+            ->assertStatus(200)
+            ->assertSee('Most Active Urls');
+
+        foreach($urlsClicked as $url) {
+            $response->assertSee($url->title)
+                ->assertSee($url->url)
+                ->assertSee($url->little_url)
+                ->assertSee($url->clicks->count() . ' Clicks');
+        }
+
+        $data = $response->getOriginalContent()->getData();
+
+        $this->assertEquals(5, $data['recentUrls']->count());
     }
 }
